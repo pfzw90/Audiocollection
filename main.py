0561e00e9f00bffc6d7f80b2c5b5f0f80a76c7ed
Natalya Chekhonina <pfzw90@gmail.com>
2020-09-28 16:22:11 +0000

    Some magic added

--- main.py
+++ main.py
@@ -2,8 +2,29 @@ class Track:
   def __init__(self, trackname, tracklength):
     self.name = trackname
     self.length = tracklength
-  def show(self):
-    print(f'Name: \"{self.name}\" | Length: {self.length} minutes')
+
+  ##  def show(self):
+  ##    print(f'Name: \"{self.name}\" | Length: {self.length} minutes')
+
+  def __str__(self):
+    return f'Name: \"{self.name}\" | Length: {self.length} minutes'
+
+  def __lt__(self, other):
+    return print(self.length < other.length)
+  
+  def __gt__(self, other):
+    return print(self.length > other.length)
+
+  def __eq__(self, other):
+    return print(self.length == other.length)
+  
+  def __le__(self, other):
+    return print(self.length <= other.length)
+
+  def __ge__(self, other):
+    return print(self.length >= other.length)
+
+  
 
 class Album: 
   def __init__(self, albumname, band):
@@ -11,10 +32,16 @@ class Album:
     self.band = band
     self.tracklist = []
 
-  def get_tracks(self):
-    print(f'{self.name}: ')
+  ##  def get_tracks(self):
+  ##    print(f'{self.name}: ')
+  ##    for track in self.tracklist:
+  ##      track.show()
+
+  def __str__(self):
+    printable_tracklist = ''
     for track in self.tracklist:
-      track.show()
+      printable_tracklist += ('\t' + track.__str__() + '\n')
+    return f'Album name: \"{self.name}\" \nAlbum author: \"{self.band}\"\nTracks: \n{printable_tracklist}'
   
   def add_track(self, trackname, tracklength):
     self.tracklist.append(Track(trackname, tracklength))
@@ -41,8 +68,17 @@ def create_audio_collection():
   album_b.add_track('The song five', 5)
   album_b.add_track('The song six', 6)
 
-  album_a.get_tracks()
-  album_b.get_tracks()
+  ##  album_a.get_tracks()
+  ##  album_b.get_tracks()
+
+  album_a.tracklist[1] > album_b.tracklist[2]
+  album_a.tracklist[1] < album_b.tracklist[2]
+  album_a.tracklist[1] == album_b.tracklist[2]
+  album_a.tracklist[1] <= album_b.tracklist[2]
+  album_a.tracklist[1] >= album_b.tracklist[2]
+
+  print(album_a)
+  print(album_b)
 
 create_audio_collection()
 print(f'Album A duration: {album_a.get_duration()} mins.')
